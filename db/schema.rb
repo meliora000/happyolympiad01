04c670c9c2e40dc7ce9507994a4490ae2958ee38
joseph lee <meliora000@github.com>
2017-01-13 16:40:51 +0900

    for heruko deploy

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 20161223053114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.text     "option"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
